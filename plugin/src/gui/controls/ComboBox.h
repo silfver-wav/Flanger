467249284d3490b5274b8a29e61b9c4656179d08
Linus Silfver <linus.silfver@gmail.com>
2024-12-16 23:58:45 +0100

    fixed ComboBox.h layout and size

--- plugin/src/gui/controls/ComboBox.h
+++ plugin/src/gui/controls/ComboBox.h
@@ -12,23 +12,24 @@ public:
     label.setJustificationType(juce::Justification::centred);
     addAndMakeVisible(label);
 
-    comboBox.setSize(comboBoxWidth, comboBoxHeight);
-    for (int i = 0; i < options.size(); ++i)
+    for (int i = 0; i < options.size(); ++i) {
       comboBox.addItem(options[i], i + 1);
+    }
+
     addAndMakeVisible(comboBox);
   }
 
   void resized() override {
     auto area = getLocalBounds();
-    label.setBounds(area.removeFromTop(labelHeight));
-    comboBox.setBounds(area.reduced(25));
+    int height = area.getHeight() / 2;
+    label.setBounds(area.removeFromTop(height));
+    comboBox.setBounds (10, height, getWidth() - comboBoxWidth, comboBoxHeight);
   }
-
   juce::ComboBox comboBox;
 private:
   juce::Label label;
   int labelWidth = 150, labelHeight = 16;
-  int comboBoxWidth = 150, comboBoxHeight = 5;
+  int comboBoxWidth = 20, comboBoxHeight = 20;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ComboBoxKnob)
 };
