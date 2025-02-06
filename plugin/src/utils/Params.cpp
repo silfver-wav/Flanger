cbe6b20c02b4e3ec3a0dd033720dfcefd81254d8
Linus Silfver <linus.silfver@gmail.com>
2025-02-06 15:17:46 +0100

    fixed stringFromValue

--- plugin/src/utils/Params.cpp
+++ plugin/src/utils/Params.cpp
@@ -7,77 +7,83 @@ Params::createParameterLayout() {
 
   std::vector<std::unique_ptr<RangedAudioParameter>> params;
 
-  params.push_back(std::make_unique<juce::AudioParameterFloat>(
+  params.push_back(std::make_unique<AudioParameterFloat>(
       ParamIDs::delay, "Delay",
-      juce::NormalisableRange(ParamRange::delayStart, ParamRange::delayEnd,
-                              ParamRange::delayInterval),
-      ParamRange::delayDefault));
+      NormalisableRange(ParamRange::delayStart, ParamRange::delayEnd,
+                        ParamRange::delayInterval),
+      ParamRange::delayDefault, String(),
+      AudioProcessorParameter::genericParameter,
+      [](float value, float) { return String(value, 1) + " ms"; }));
 
-  params.push_back(std::make_unique<juce::AudioParameterFloat>(
+  params.push_back(std::make_unique<AudioParameterFloat>(
       ParamIDs::feedback, "Feedback",
-      juce::NormalisableRange(ParamRange::feedbackStart,
-                              ParamRange::feedbackEnd),
-      ParamRange::feedbackDefault));
+      NormalisableRange(ParamRange::feedbackStart, ParamRange::feedbackEnd),
+      ParamRange::feedbackDefault, String(),
+      AudioProcessorParameter::genericParameter,
+      [](float value, float) { return String(static_cast<int>(value * 100)) + " %"; }));
 
-  params.push_back(std::make_unique<juce::AudioParameterBool>(
+  params.push_back(std::make_unique<AudioParameterBool>(
       ParamIDs::invertPolarity, "Invert Polarity", false));
 
-  params.push_back(std::make_unique<juce::AudioParameterBool>(
-      ParamIDs::invertWet, "Invert Wet", false));
+  params.push_back(std::make_unique<AudioParameterBool>(ParamIDs::invertWet,
+                                                        "Invert Wet", false));
 
-  params.push_back(std::make_unique<juce::AudioParameterFloat>(
+  params.push_back(std::make_unique<AudioParameterFloat>(
       ParamIDs::lfoFreq, "LFO Frequency",
-      juce::NormalisableRange(ParamRange::lfoFreqStart, ParamRange::lfoFreqEnd,
-                              ParamRange::lfoFreqInterval),
-      ParamRange::lfoFreqDefault));
+      NormalisableRange(ParamRange::lfoFreqStart, ParamRange::lfoFreqEnd,
+                        ParamRange::lfoFreqInterval),
+      ParamRange::lfoFreqDefault, String(),
+      AudioProcessorParameter::genericParameter,
+      [](float value, float) { return String(value, 0.01) + " Hz"; }));
 
-  params.push_back(std::make_unique<juce::AudioParameterChoice>(
+  params.push_back(std::make_unique<AudioParameterChoice>(
       ParamIDs::lfoSyncMode, "LFO Sync Mode",
-      juce::StringArray{"Frequency", "Tempo-Synced"}, 0));
+      StringArray{"Frequency", "Tempo-Synced"}, 0));
 
-    params.push_back(std::make_unique<juce::AudioParameterChoice>(
-        ParamIDs::lfoRate,
-        "LFO Rate",
-        ParamRange::lfoRates,
-        2));
+  params.push_back(std::make_unique<AudioParameterChoice>(
+      ParamIDs::lfoRate, "LFO Rate", ParamRange::lfoRates, 2));
 
-  params.push_back(std::make_unique<juce::AudioParameterFloat>(
+  params.push_back(std::make_unique<AudioParameterFloat>(
       ParamIDs::lfoDepth, "LFO Depth",
-      juce::NormalisableRange(ParamRange::lfoDepthStart,
-                              ParamRange::lfoDepthEnd,
-                              ParamRange::lfoDepthInterval),
+      NormalisableRange(ParamRange::lfoDepthStart, ParamRange::lfoDepthEnd,
+                        ParamRange::lfoDepthInterval),
       ParamRange::lfoDepthDefault));
 
-  params.push_back(std::make_unique<juce::AudioParameterChoice>(
+  params.push_back(std::make_unique<AudioParameterChoice>(
       ParamIDs::waveForm, "LFO Wave Form", ParamRange::waveformChoices, 0));
 
-  params.push_back(std::make_unique<juce::AudioParameterFloat>(
+  params.push_back(std::make_unique<AudioParameterFloat>(
       ParamIDs::stereo, "Stereo",
-      juce::NormalisableRange<float>(ParamRange::stereoStart,
-                                     ParamRange::stereoEnd,
-                                     ParamRange::stereoInterval),
-      ParamRange::stereoDefault));
+      NormalisableRange(ParamRange::stereoStart, ParamRange::stereoEnd,
+                        ParamRange::stereoInterval),
+      ParamRange::stereoDefault, String(),
+      AudioProcessorParameter::genericParameter,
+      [](float value, float) { return String(static_cast<int>(value * 100)) + " %"; }));
 
-  params.push_back(std::make_unique<juce::AudioParameterFloat>(
+  params.push_back(std::make_unique<AudioParameterFloat>(
       ParamIDs::mix, "Mix",
-      juce::NormalisableRange(ParamRange::mixStart, ParamRange::mixEnd,
-                              ParamRange::mixInterval),
-      ParamRange::mixDefault));
+      NormalisableRange(ParamRange::mixStart, ParamRange::mixEnd,
+                        ParamRange::mixInterval),
+      ParamRange::mixDefault, String(),
+      AudioProcessorParameter::genericParameter,
+      [](float value, float) { return String(static_cast<int>(value * 100)) + " %"; }));
 
-    params.push_back(std::make_unique<juce::AudioParameterFloat>(
-        ParamIDs::saturation, "Saturation",
-        juce::NormalisableRange(ParamRange::saturationStart, ParamRange::saturationEnd,
-                                ParamRange::saturationInterval),
-        ParamRange::saturationDefault));
+  params.push_back(std::make_unique<AudioParameterFloat>(
+      ParamIDs::saturation, "Saturation",
+      NormalisableRange(ParamRange::saturationStart, ParamRange::saturationEnd,
+                        ParamRange::saturationInterval),
+      ParamRange::saturationDefault));
 
   params.push_back(std::make_unique<AudioParameterFloat>(
       ParamIDs::gain, "Gain",
-      NormalisableRange<float>(ParamRange::gainStart, ParamRange::gainEnd,
-                               ParamRange::gainInterval),
-      ParamRange::gainDefault));
+      NormalisableRange(ParamRange::gainStart, ParamRange::gainEnd,
+                        ParamRange::gainInterval),
+      ParamRange::gainDefault, String(),
+      AudioProcessorParameter::genericParameter,
+      [](float value, float) { return String(value, 1) + " dB "; }));
 
-  params.push_back(std::make_unique<juce::AudioParameterBool>(ParamIDs::power,
-                                                              "Power", true));
+  params.push_back(
+      std::make_unique<AudioParameterBool>(ParamIDs::power, "Power", true));
 
   return {params.begin(), params.end()};
 }
